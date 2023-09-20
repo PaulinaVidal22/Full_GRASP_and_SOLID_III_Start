2b08b2e893c39565fdf71edb59bb9b012ad3f83c
PauloinaVidal22 <paulina.vidal@correo.ucu.edu.uy>
2023-09-20 17:26:53 -0300

    creación de la interfaz  IPrinter para establecer el tipo para el ConsolePrinter y FilePrinter (dos clases implementadas) y que el printRecipe sea una operación de polimorfica

---
+++ src/Library/IPrinter.cs
@@ -0,0 +1,9 @@
+namespace Full_GRASP_And_SOLID.Library
+{
+    public interface IPrinter
+    {
+        void PrintRecipe (Recipe recipe);
+    }
+}
+
+//para realizar este cambio me baso en el principio de Polymorfism que forma parte de los principios GRASP.
